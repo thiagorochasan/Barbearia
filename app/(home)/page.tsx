3190baa0e03ae9a1232c4e022911296060c834aa
thiagorochasan <thiagorochasan@gmail.com>
2025-07-04 20:20:32 -0300

    Cancelar agendamentos

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,18 +5,39 @@ import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../_lib/auth";
 
 
 export default async function Home() {
 
-    const barbershops = await db.barbershop.findMany({});
+    const session = await getServerSession(authOptions);
+
+    const [barbershops, confirmedBookings] = await Promise.all([
+
+        db.barbershop.findMany({}),
+
+        session?.user
+            ? db.booking.findMany({
+                where: {
+                    userId: (session.user as any).id,
+                    date: {
+                        gte: new Date(),
+                    }
+                },
+                include: {
+                    service: true,
+                    barbershop: true
+                }
+            }) : Promise.resolve([])
+    ])
 
     return (
 
         <div>
             <Header></Header>
             <div className="px-5 pt-5">
-        
+
                 <h2 className="text-xl font-bold">Olá, Thiago!</h2>
                 <p className="capitalize text-sm">
                     {format(new Date(), "EEEE',' dd 'de' MMMM", {
@@ -28,11 +49,20 @@ export default async function Home() {
                 <Search />
             </div>
 
-            {/* <div className="px-5 mt-6">
+            <div className="px-5 mt-6">
                 <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
-                <BookingItem/>
-            </div> */}
-            
+
+                <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+
+                    {confirmedBookings.map((booking) => (
+                        <BookingItem key={booking.id} booking={booking} />
+                    ))}
+
+                </div>
+
+
+            </div>
+
             <div className="mt-6">
                 <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
 
--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -13,20 +13,42 @@ const BookingsPage = async () => {
     return redirect("/");
   }
 
-  const bookings = await db.booking.findMany({
-    where: {
-      userId: (session.user as any).id
-    },
-    include: {
-      service: true,
-      barbershop: true,
-    }
+  const [confirmedBookings, finishedBookings] = await Promise.all([
+    db.booking.findMany({
+      where: {
+        userId: (session.user as any).id,
+        date: {
+          gte: new Date(),
+        }
+      },
+      include: {
+        service: true,
+        barbershop: true,
+      }
 
-  })
+    }),
+    db.booking.findMany({
+      where: {
+        userId: (session.user as any).id,
+        date: {
+          lt: new Date(),
+        }
+      },
+      include: {
+        service: true,
+        barbershop: true,
+      }
+
+    })
+  ])
 
 
-  const confirmedBookings = bookings.filter(booking => isFuture(booking.date));
-  const finishedBookings = bookings.filter(booking => isPast(booking.date));
+
+
+
+
+  //const confirmedBookings = bookings.filter(booking => isFuture(booking.date));
+  //const finishedBookings = bookings.filter(booking => isPast(booking.date));
 
   return (
     <>
@@ -35,24 +57,28 @@ const BookingsPage = async () => {
       <div className="px-5 py-6">
         <h1 className="text-xl font-bold mb-6">Agendamentos</h1>
 
-        <h2 className="text-gray-400 uppercase font-bold text-sm mb-3">Confirmados</h2>
+        {confirmedBookings.length > 0 && (
+          <>
+            <h2 className="text-gray-400 uppercase font-bold text-sm mb-3">Confirmados</h2>
+            <div className="flex flex-col gap-3">
+              {confirmedBookings.map((booking) => (
+                <BookingItem key={booking.id} booking={booking} />
+              ))}
+            </div>
+          </>
+        )}
 
-        <div className="flex flex-col gap-3">
-          {confirmedBookings.map((booking) => (
-            <BookingItem key={booking.id} booking={booking} />
-          ))}
-        </div>
+        {finishedBookings.length > 0 && (
+          <>
+            <h2 className="text-gray-400 uppercase font-bold text-sm mb-3">Finalizados</h2>
 
-
-        <h2 className="text-gray-400 uppercase font-bold text-sm mb-3">Finalizados</h2>
-
-        <div className="flex flex-col gap-3">
-          {finishedBookings.map((booking) => (
-            <BookingItem key={booking.id} booking={booking} />
-          ))}
-        </div>
-
-
+            <div className="flex flex-col gap-3">
+              {finishedBookings.map((booking) => (
+                <BookingItem key={booking.id} booking={booking} />
+              ))}
+            </div>
+          </>
+        )}
 
       </div>
     </>
