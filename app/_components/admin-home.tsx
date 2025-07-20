b2f0b4a94fbab666cfc02bee8b3cff10f5aa0d42
thiagorochasan <thiagorochasan@gmail.com>
2025-07-19 22:03:45 -0300

    Correção nas hpras e minutos

--- app/_components/admin-home.tsx
+++ app/_components/admin-home.tsx
@@ -21,45 +21,8 @@ export default async function AdminHome() {
         return <p className="p-5">Você não tem permissão para acessar esta página.</p>;
     }
 
-    // const startOfDay = new Date();
-    // startOfDay.setHours(0, 0, 0, 0);
-
-    // const endOfDay = new Date();
-    // endOfDay.setHours(23, 59, 59, 999);
-    // endOfDay.setDate(endOfDay.getDate() + 1); // Adiciona 1 dia
-
-    // Buscar agendamentos da barbearia desse admin no dia atual
-    // const bookings = await db.booking.findMany({
-    //     where: {
-    //         barbershopId: user.barbershopId,
-    //         date: {
-    //             gte: startOfDay,
-    //             lte: endOfDay,
-    //         },
-    //     },
-    //     include: {
-    //         user: true,
-    //         service: true,
-    //     },
-    //     orderBy: {
-    //         date: "asc",
-    //     },
-    //  });
-
     return (
 
-            <AdminSchedule barbershopId={user.barbershopId} />
-
-        // <div className="p-5">
-        //     <h1 className="text-2xl font-bold mb-4">Agendamentos do Dia</h1>
-
-        //     {bookings.length === 0 && <p>Não há agendamentos para hoje.</p>}
-
-        //     <ul className="space-y-4">
-        //         {bookings.map((booking) => (
-        //             <BookingItemAdmin key={booking.id} booking={booking} />
-        //         ))}
-        //     </ul>
-        // </div>
+        <AdminSchedule barbershopId={user.barbershopId} />
     );
 }
--- app/_components/admin-schedule.tsx
+++ app/_components/admin-schedule.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { format, startOfDay, endOfDay, setMinutes, setHours, isSameHour } from "date-fns";
+import { format, startOfDay, endOfDay, setMinutes, setHours, isSameHour, isEqual } from "date-fns";
 import { Calendar } from "./ui/calendar";
 import { ptBR } from "date-fns/locale/pt-BR";
 import WeekSelector from "./week-selector";
@@ -24,7 +24,14 @@ export default function AdminSchedule({ barbershopId }: { barbershopId: string }
     const [loading, setLoading] = useState(true);
     const [dayBookings, setDayBookings] = useState<Booking[]>([]);
 
-    const hours = Array.from({ length: 14 }, (_, i) => 8 + i) // 08h às 21h
+    // const hours = Array.from({ length: 14 }, (_, i) => 8 + i) // 08h às 21h
+
+    // console.log(hours);
+
+
+    const hours = Array.from({ length: 28 }, (_, i) => 8 + i * 0.5);
+
+    //console.log(hours2);
 
     useEffect(() => {
         const fetchBookings = async () => {
@@ -54,21 +61,27 @@ export default function AdminSchedule({ barbershopId }: { barbershopId: string }
             ) : (
                 <div className="space-y-3">
                     {hours.map((hour) => {
-                        const slot = setMinutes(setHours(selectedDate, hour), 0)
+                        const wholeHour = Math.floor(hour); // ex: 8
+                        const minutes = hour % 1 === 0.5 ? 30 : 0; // ex: 30 se for 8.5
+
+                        const slot = setMinutes(setHours(selectedDate, wholeHour), minutes);
+
+
                         const booking = dayBookings.find((b) =>
-                            isSameHour(new Date(b.date), slot)
-                        )
+                            isEqual(new Date(b.date), slot)
+                        );
 
                         return (
                             <div key={hour} className="flex items-start space-x-4">
-                                <div className="w-16 text-sm text-muted-foreground">{format(slot, "HH:mm")}</div>
+                                <div className="w-16 text-sm text-muted-foreground">
+                                    {format(slot, "HH:mm")}
+                                </div>
                                 {booking ? (
                                     <BookingItemAdmin booking={booking} />
                                 ) : (
                                     <Card className="min-w-full">
-
-                                        <CardContent className="p-3 text-sm italic text-muted-foreground">Horário livre
-
+                                        <CardContent className="p-3 text-sm italic text-muted-foreground">
+                                            Horário livre
                                         </CardContent>
                                     </Card>
                                 )}
