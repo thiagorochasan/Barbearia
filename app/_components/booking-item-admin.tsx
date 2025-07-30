0e146c2113c936f7c271303cd3e78cf930dcf230
thiagorochasan <thiagorochasan@gmail.com>
2025-07-30 15:35:14 -0300

    Correção no item-admin

--- app/_components/booking-item-admin.tsx
+++ app/_components/booking-item-admin.tsx
@@ -50,7 +50,7 @@ const BookingItemAdmin = ({ booking }: BookingItemProps) => {
             <SheetTrigger asChild>
                 <Card className="min-w-full">
                     <CardContent className="py-0 flex px-0">
-                        <div className="flex flex-col gap-1 py-5 flex-[3] pl-5">
+                        <div className="flex flex-col gap-1 py-2 flex-[3] pl-5">
                              <Badge variant={isBookingConfirmed ? 'default' : 'secondary'} className="w-fit" >
 
                                 {isBookingConfirmed ? 'Confirmado' : 'Finalizado'}
